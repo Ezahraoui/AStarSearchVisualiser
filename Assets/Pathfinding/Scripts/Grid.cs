ad3bd2215ae6c7ad61d4bae2e47a3be6efd7aa6f
J.Ezahraoui <ezahraouijad@gmail.com>
2020-11-02 05:16:34 +0100

    prepare the two dimensional grid

---
+++ Assets/Pathfinding/Scripts/Grid.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Code.Utils;
+
+public class Grid
+{
+    private int width;
+    private int height;
+    private float cellSize;
+    private int[,] twoDimArray;
+    public Grid(int width, int height, float cellSize)
+    {
+        this.width = width;
+        this.height = height;
+        this.cellSize = cellSize;
+        twoDimArray = new int[width, height];
+        //Debug.Log(width + " " + height);
+
+        for(int i=0; i< twoDimArray.GetLength(0); i++)
+        {
+            for(int j = 0; j < twoDimArray.GetLength(1); j++)
+            {
+                UtilsClass.CreateWorldText(twoDimArray[i,j].ToString(), null, GetWorldPosition(i,j), 20, Color.white, TextAnchor.MiddleCenter);
+                Debug.DrawLine(GetWorldPosition(i, j), GetWorldPosition(i, j + 1), Color.white, 100f);
+                Debug.DrawLine(GetWorldPosition(i, j), GetWorldPosition(i+1, j), Color.white, 100f);
+            }
+        }
+    }
+    private Vector3 GetWorldPosition(int i, int j)
+    {
+        return new Vector3(i, j) * cellSize;
+    }
+}
